e8b36bd8aebb90f3a02a04691a7d30acf0559a2c
nathan <nathan_lopes147@hotmail.com>
2023-12-15 11:35:10 -0300

    02-CRUD-Produto

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,73 @@
+package com.generation.lojagames.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Long id;
+	
+	@NotBlank(message = "O Atributo nome é obrigatório!")
+	private String nome;
+	
+	private String plataforma;
+	
+	private Double preco;
+	
+	@Column(length=1000)
+	@Size(max=1000, message = "O atríbuto foto pode ter no máximo 1000 caracteres.")
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	
+	
+}
